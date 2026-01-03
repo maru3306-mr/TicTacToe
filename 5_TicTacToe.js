6be5c06181b8bde71ff0cf09f31149a794495f32
maru3306-mr <markus.rudel93@gmail.com>
2026-01-03 16:07:40 +0100

    Add files via upload
    added styling

--- 5_TicTacToe.js
+++ 5_TicTacToe.js
@@ -40,15 +40,15 @@ y↓   ┌──────┬──────┬──────┐
         //checks if field is free or occupied already with a symbol		
         return this.board[y][x] === null;
     }
-	placeSymbol(x, y, symbol) {
-	if (this.board[y][x] !== null) {
-		console.log("Feld belegt");
-		return false;
-	}
+    placeSymbol(x, y, symbol) {
+        if (this.board[y][x] !== null) {
+            console.log("Field occupied");
+            return false;
+        }
 
-	this.board[y][x] = symbol;
-	return true;
-	}
+        this.board[y][x] = symbol;
+        return true;
+    }
 
 
 }
@@ -127,7 +127,7 @@ class Game {
             return true;
 
         } else {
-            console.log('Es geht weiter!');
+            console.log('Game continues!');
             return false;
         }
     }
@@ -183,188 +183,329 @@ myBoard.printBoard();
 
 //defines the starting condition of the game 
 var currentPlayer = "X";
+var gameOver = false;
 
 var player = document.getElementById("player_info");
 var symbol = document.getElementById("player_symbol");
-	
+
 
 player.textContent = "Player : " + currentPlayer;
 
 
 /*Grab HTML stuff
 Basically gets every value of each HTML div id from field 00 to field 22 including 
-the dataset- values defined for each field in the HTML
+the dataset- values defined for each field in the HTML and adds logic to it
+
+// to be done:
+put it into loop to get rid of redundant code 
 
 */
 
 var x0y0 = document.getElementById("field00");
 const x00 = Number(x0y0.dataset.x);
 const y00 = Number(x0y0.dataset.y);
-x0y0.onclick = function(){
-	console.log("X: " + x00 + " Y: " + y00);
+x0y0.onclick = function() {
+    console.log("X: " + x00 + " Y: " + y00);
+    if (gameOver === true) {
+        ///checks if game is over and stops the possibilty to click in the fields
+        return;
+    }
+    if (myBoard.placeSymbol(x00, y00, currentPlayer) === true) {
+        // myBoard.placeSymbol(x00, y00, currentPlayer);
+        x0y0.textContent = currentPlayer;
+        var winner;
+        winner = currentPlayer;
 
-	myBoard.placeSymbol(x00,y00,currentPlayer);
-	x0y0.textContent=currentPlayer;
-	if (currentPlayer === "X") {
-		currentPlayer = "O";
-		player.textContent = "Player : " + currentPlayer;
-		} else {
-		currentPlayer = "X";
-		player.textContent = "Player : " + currentPlayer;
-		}
-	
-	myBoard.printBoard();
-	myGame.checkGame(myBoard.board);
+        if (currentPlayer === "X") {
+            currentPlayer = "O";
+            player.textContent = "Player : " + currentPlayer;
+        } else {
+            currentPlayer = "X";
+            player.textContent = "Player : " + currentPlayer;
+        }
+
+        if (myGame.checkGame(myBoard.board) !== true) {
+            console.log("Game is running")
+        } else {
+            console.log("Game ended")
+            player.textContent = "The Winner is : " + winner;
+            gameOver = true;
+            document.getElementById("board").classList.add("locked");
+        }
+    }
+
+
 }
 
 var x1y0 = document.getElementById("field01");
 const x10 = Number(x1y0.dataset.x);
 const y10 = Number(x1y0.dataset.y);
-x1y0.onclick = function(){
-	console.log("X: " + x10 + " Y: " + y10);
+x1y0.onclick = function() {
+    console.log("X: " + x10 + " Y: " + y10);
+    if (gameOver === true) {
+        ///checks if game is over and stops the possibilty to click in the fields
+        return;
+    }
 
-	myBoard.placeSymbol(x10,y10,currentPlayer);
-	myBoard.printBoard();
-	x1y0.textContent=currentPlayer;
-		if (currentPlayer === "X") {
-		currentPlayer = "O";
-		player.textContent = currentPlayer;
-		} else {
-		currentPlayer = "X";
-		player.textContent = currentPlayer;
-		}
-	myGame.checkGame(myBoard.board);
-	
+    if (myBoard.placeSymbol(x10, y10, currentPlayer) === true) {
+        // myBoard.placeSymbol(x10, y10, currentPlayer);
+        myBoard.printBoard();
+        x1y0.textContent = currentPlayer;
+        var winner;
+        winner = currentPlayer;
+
+        if (currentPlayer === "X") {
+            currentPlayer = "O";
+            player.textContent = "Player : " + currentPlayer;
+        } else {
+            currentPlayer = "X";
+            player.textContent = "Player : " + currentPlayer;
+
+        }
+        if (myGame.checkGame(myBoard.board) !== true) {
+            console.log("Game is running")
+        } else {
+            console.log("Game ended")
+            player.textContent = "The Winner is : " + winner;
+            gameOver = true;
+            document.getElementById("board").classList.add("locked");
+        }
+
+    }
+
 }
 
 var x2y0 = document.getElementById("field02");
 const x20 = Number(x2y0.dataset.x);
 const y20 = Number(x2y0.dataset.y);
-x2y0.onclick = function(){
-	console.log("X: " + x20 + " Y: " + y20);
-	myBoard.placeSymbol(x20,y20,currentPlayer);
-	myBoard.printBoard();	
-	x2y0.textContent=currentPlayer;
-		if (currentPlayer === "X") {
-		currentPlayer = "O";
-		player.textContent = currentPlayer;
-		} else {
-		currentPlayer = "X";
-		player.textContent = currentPlayer;
-		}
-	myGame.checkGame(myBoard.board);
+x2y0.onclick = function() {
+    console.log("X: " + x20 + " Y: " + y20);
+    if (gameOver === true) {
+        ///checks if game is over and stops the possibilty to click in the fields
+        return;
+    }
+    if (myBoard.placeSymbol(x20, y20, currentPlayer) === true) {
+        // myBoard.placeSymbol(x20, y20, currentPlayer);
+        myBoard.printBoard();
+        x2y0.textContent = currentPlayer;
+        var winner;
+        winner = currentPlayer;
+        if (currentPlayer === "X") {
+            currentPlayer = "O";
+            player.textContent = "Player : " + currentPlayer;
+        } else {
+            currentPlayer = "X";
+            player.textContent = "Player : " + currentPlayer;
+        }
+        if (myGame.checkGame(myBoard.board) !== true) {
+            console.log("Game is running")
+        } else {
+            console.log("Game ended")
+            player.textContent = "The Winner is : " + winner;
+            gameOver = true;
+            document.getElementById("board").classList.add("locked");
+        }
+    }
 }
 
 var x0y1 = document.getElementById("field10");
 const x01 = Number(x0y1.dataset.x);
 const y01 = Number(x0y1.dataset.y);
-x0y1.onclick = function(){
-	console.log("X: " + x01 + " Y: " + y01);
-	myBoard.placeSymbol(x01,y01,currentPlayer);
-	myBoard.printBoard();
-	x0y1.textContent=currentPlayer;
-		if (currentPlayer === "X") {
-		currentPlayer = "O";
-		player.textContent = currentPlayer;
-		} else {
-		currentPlayer = "X";
-		player.textContent = currentPlayer;
-		}
-	myGame.checkGame(myBoard.board);
+x0y1.onclick = function() {
+    console.log("X: " + x01 + " Y: " + y01);
+    if (gameOver === true) {
+        ///checks if game is over and stops the possibilty to click in the fields
+        return;
+    }
+    if (myBoard.placeSymbol(x01, y01, currentPlayer) === true) {
+        //myBoard.placeSymbol(x01, y01, currentPlayer);
+        myBoard.printBoard();
+        x0y1.textContent = currentPlayer;
+        var winner;
+        winner = currentPlayer;
+        if (currentPlayer === "X") {
+            currentPlayer = "O";
+            player.textContent = "Player : " + currentPlayer;
+        } else {
+            currentPlayer = "X";
+            player.textContent = "Player : " + currentPlayer;
+        }
+        if (myGame.checkGame(myBoard.board) !== true) {
+            console.log("Game is running")
+        } else {
+            console.log("Game ended")
+            player.textContent = "The Winner is : " + winner;
+            gameOver = true;
+            document.getElementById("board").classList.add("locked");
+        }
+    }
 }
 
 var x1y1 = document.getElementById("field11");
 const x11 = Number(x1y1.dataset.x);
 const y11 = Number(x1y1.dataset.y);
-x1y1.onclick = function(){
-	console.log("X: " + x11 + " Y: " + y11);
-	myBoard.placeSymbol(x11,y11,currentPlayer);
-	myBoard.printBoard();
-	x1y1.textContent=currentPlayer;
-		if (currentPlayer === "X") {
-		currentPlayer = "O";
-		player.textContent = currentPlayer;
-		} else {
-		currentPlayer = "X";
-		player.textContent = currentPlayer;
-		}
-	myGame.checkGame(myBoard.board);
+x1y1.onclick = function() {
+    console.log("X: " + x11 + " Y: " + y11);
+    if (gameOver === true) {
+        ///checks if game is over and stops the possibilty to click in the fields
+        return;
+    }
+    if (myBoard.placeSymbol(x11, y11, currentPlayer) === true) {
+        //myBoard.placeSymbol(x11, y11, currentPlayer);
+        myBoard.printBoard();
+        x1y1.textContent = currentPlayer;
+        var winner;
+        winner = currentPlayer;
+        if (currentPlayer === "X") {
+            currentPlayer = "O";
+            player.textContent = "Player : " + currentPlayer;
+        } else {
+            currentPlayer = "X";
+            player.textContent = "Player : " + currentPlayer;
+        }
+        if (myGame.checkGame(myBoard.board) !== true) {
+            console.log("Game is running")
+        } else {
+            console.log("Game ended")
+            player.textContent = "The Winner is : " + winner;
+            gameOver = true;
+            document.getElementById("board").classList.add("locked");
+        }
+    }
 }
 
 var x2y1 = document.getElementById("field12");
 const x21 = Number(x2y1.dataset.x);
 const y21 = Number(x2y1.dataset.y);
-x2y1.onclick = function(){
-	console.log("X: " + x21 + " Y: " + y21);
-	myBoard.placeSymbol(x21,y21,currentPlayer);
-	myBoard.printBoard();
-	x2y1.textContent=currentPlayer;
-		if (currentPlayer === "X") {
-		currentPlayer = "O";
-		player.textContent = currentPlayer;
-		} else {
-		currentPlayer = "X";
-		player.textContent = currentPlayer;
-		}
-	myGame.checkGame(myBoard.board);
+x2y1.onclick = function() {
+    console.log("X: " + x21 + " Y: " + y21);
+    if (gameOver === true) {
+        ///checks if game is over and stops the possibilty to click in the fields
+        return;
+    }
+    if (myBoard.placeSymbol(x21, y21, currentPlayer) === true) {
+        //myBoard.placeSymbol(x21, y21, currentPlayer);
+        myBoard.printBoard();
+        x2y1.textContent = currentPlayer;
+        var winner;
+        winner = currentPlayer;
+        if (currentPlayer === "X") {
+            currentPlayer = "O";
+            player.textContent = "Player : " + currentPlayer;
+        } else {
+            currentPlayer = "X";
+            player.textContent = "Player : " + currentPlayer;
+        }
+        if (myGame.checkGame(myBoard.board) !== true) {
+            console.log("Game is running")
+        } else {
+            console.log("Game ended")
+            player.textContent = "The Winner is : " + winner;
+            gameOver = true;
+            document.getElementById("board").classList.add("locked");
+        }
+    }
 }
 
 var x0y2 = document.getElementById("field20");
 const x02 = Number(x0y2.dataset.x);
 const y02 = Number(x0y2.dataset.y);
-x0y2.onclick = function(){
-	console.log("X: " + x02 + " Y: " + y02);
-	myBoard.placeSymbol(x02,y02,currentPlayer);
-	myBoard.printBoard();
-	x0y2.textContent=currentPlayer;
-		if (currentPlayer === "X") {
-		currentPlayer = "O";
-		player.textContent = currentPlayer;
-		} else {
-		currentPlayer = "X";
-		player.textContent = currentPlayer;
-		}
-	myGame.checkGame(myBoard.board);
+x0y2.onclick = function() {
+    console.log("X: " + x02 + " Y: " + y02);
+    if (gameOver === true) {
+        ///checks if game is over and stops the possibilty to click in the fields
+        return;
+    }
+    if (myBoard.placeSymbol(x02, y02, currentPlayer) === true) {
+        // myBoard.placeSymbol(x02, y02, currentPlayer);
+        myBoard.printBoard();
+        x0y2.textContent = currentPlayer;
+        var winner;
+        winner = currentPlayer;
+        if (currentPlayer === "X") {
+            currentPlayer = "O";
+            player.textContent = "Player : " + currentPlayer;
+        } else {
+            currentPlayer = "X";
+            player.textContent = "Player : " + currentPlayer;
+        }
+        if (myGame.checkGame(myBoard.board) !== true) {
+            console.log("Game is running")
+        } else {
+            console.log("Game ended")
+            player.textContent = "The Winner is : " + winner;
+            gameOver = true;
+            document.getElementById("board").classList.add("locked");
+        }
+    }
 }
 
 var x1y2 = document.getElementById("field21");
 const x12 = Number(x1y2.dataset.x);
 const y12 = Number(x1y2.dataset.y);
-x1y2.onclick = function(){
-	console.log("X: " + x12 + " Y: " + y12);
-	myBoard.placeSymbol(x12,y12,currentPlayer);
-	myBoard.printBoard();
-	x1y2.textContent=currentPlayer;
-		if (currentPlayer === "X") {
-		currentPlayer = "O";
-		player.textContent = currentPlayer;
-		} else {
-		currentPlayer = "X";
-		player.textContent = currentPlayer;
-		}
-	myGame.checkGame(myBoard.board);
+x1y2.onclick = function() {
+    console.log("X: " + x12 + " Y: " + y12);
+    if (gameOver === true) {
+        ///checks if game is over and stops the possibilty to click in the fields
+        return;
+    }
+    if (myBoard.placeSymbol(x12, y12, currentPlayer) === true) {
+        //myBoard.placeSymbol(x12, y12, currentPlayer);
+        myBoard.printBoard();
+        x1y2.textContent = currentPlayer;
+        var winner;
+        winner = currentPlayer;
+        if (currentPlayer === "X") {
+            currentPlayer = "O";
+            player.textContent = "Player : " + currentPlayer;
+        } else {
+            currentPlayer = "X";
+            player.textContent = "Player : " + currentPlayer;
+        }
+        if (myGame.checkGame(myBoard.board) !== true) {
+            console.log("Game is running")
+        } else {
+            console.log("Game ended")
+            player.textContent = "The Winner is : " + winner;
+            gameOver = true;
+            document.getElementById("board").classList.add("locked");
+        }
+    }
 }
 
 var x2y2 = document.getElementById("field22");
 const x22 = Number(x2y2.dataset.x);
 const y22 = Number(x2y2.dataset.y);
-x2y2.onclick = function(){
-	console.log("X: " + x22 + " Y: " + y22);
-	myBoard.placeSymbol(x22,y22,currentPlayer);
-	myBoard.printBoard();
-	x2y2.textContent=currentPlayer;
-		if (currentPlayer === "X") {
-		currentPlayer = "O";
-		player.textContent = currentPlayer;
-		} else {
-		currentPlayer = "X";
-		player.textContent = currentPlayer;
-		}
-	myGame.checkGame(myBoard.board);
+x2y2.onclick = function() {
+    console.log("X: " + x22 + " Y: " + y22);
+    if (gameOver === true) {
+        ///checks if game is over and stops the possibilty to click in the fields
+        return;
+    }
+    if (myBoard.placeSymbol(x22, y22, currentPlayer) === true) {
+        //myBoard.placeSymbol(x22, y22, currentPlayer);
+        myBoard.printBoard();
+        x2y2.textContent = currentPlayer;
+        var winner;
+        winner = currentPlayer;
+        if (currentPlayer === "X") {
+            currentPlayer = "O";
+            player.textContent = "Player : " + currentPlayer;
+        } else {
+            currentPlayer = "X";
+            player.textContent = "Player : " + currentPlayer;
+        }
+        if (myGame.checkGame(myBoard.board) !== true) {
+            console.log("Game is running")
+        } else {
+            console.log("Game ended")
+            player.textContent = "The Winner is : " + winner;
+            gameOver = true;
+            document.getElementById("board").classList.add("locked");
+        }
+    }
+
 }
-
-
-	myBoard.printBoard();
 
 
 
@@ -394,4 +535,3 @@ do {
 
 
 */
-console.log("Gewonnen");
